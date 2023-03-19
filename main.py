6adceca7c6166f0f0c1688e9e20e7e6d2a2b765e
Manish Malasane <mmalasane6@gmail.com>
2023-03-19 16:07:30 +0530

    flask by registering the blueprints

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from api_1.starwar_app import starwar_api
+from api_1.starwar_app import starwar_api_
 from api_2.xkcd_app import xkcd_api
 
 app = Flask(__name__)
 
-app.register_blueprint(starwar_api)
+app.register_blueprint(starwar_api_)
 app.register_blueprint(xkcd_api)
-
