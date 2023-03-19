a5864cc65071b987b1005138c11b3e81ad38edc4
Manish Malasane <mmalasane6@gmail.com>
2023-03-19 16:01:59 +0530

    response body validation

---
+++ response_validation/response.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional, Union
+
+
+class PostResponse(BaseModel):
+    Record_Count: Union[str, int]
+    Name: str
+    Message: Optional[str]
+
+    # @validator("record_count")
+    # def check_records_count(cls, record_count):
+    #     if isinstance(record_count, int) or isinstance(record_count, str):
+    #         return int(record_count)
