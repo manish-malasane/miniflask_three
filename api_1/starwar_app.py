ea4a4700086ba2f99715948f1a7a94ea97330733
Manish Malasane <mmalasane6@gmail.com>
2023-03-22 12:08:36 +0530

    Registered Blueprint

--- api_1/starwar_app.py
+++ api_1/starwar_app.py
@@ -1,13 +1,19 @@
 from flask import Blueprint
-from api_1.http_requests.delete_ import delete_endpoint
-from api_1.http_requests.get_ import get_endpoint
-from api_1.http_requests.post_ import post_endpoint
+from api_1.http_requests.characters_ import characters_app
+from api_1.http_requests.films_ import film_app
+from api_1.http_requests.planets_ import planets_app
+from api_1.http_requests.vehicles_ import vehicles_app
+from api_1.http_requests.starships_ import starships_app
+from api_1.http_requests.species_ import species_app
 
 starwar_api_ = Blueprint("starwars", __name__)
 
-starwar_api_.register_blueprint(get_endpoint)
-starwar_api_.register_blueprint(post_endpoint)
-starwar_api_.register_blueprint(delete_endpoint)
+starwar_api_.register_blueprint(vehicles_app)
+starwar_api_.register_blueprint(planets_app)
+starwar_api_.register_blueprint(characters_app)
+starwar_api_.register_blueprint(starships_app)
+starwar_api_.register_blueprint(film_app)
+starwar_api_.register_blueprint(species_app)
 
 
 @starwar_api_.route("/welcome")
